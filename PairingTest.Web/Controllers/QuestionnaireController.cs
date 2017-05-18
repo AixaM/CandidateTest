1823b66c975729e6be4363a34c53e82b84423011
Ayman-PC\Aixa <aixmarg@gmail.com>
2017-05-18 07:39:06 +0100

    create call to webapi

--- PairingTest.Web/Controllers/QuestionnaireController.cs
+++ PairingTest.Web/Controllers/QuestionnaireController.cs
@@ -1,18 +1,44 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Web.Mvc;
 using PairingTest.Web.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace PairingTest.Web.Controllers
 {
     public class QuestionnaireController : Controller
     {
-          /* ASYNC ACTION METHOD... IF REQUIRED... */
-//        public async Task<ViewResult> Index()
-//        {
-//        }
-
-        public ViewResult Index()
+        public async Task<ViewResult> Index()
         {
-            return View(new QuestionnaireViewModel());
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConfigurationManager.AppSettings["QuestionnaireServiceBaseAddress"]);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/questions/");
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<QuestionnaireViewModel>();
+                   if (result != null)
+                       return View(result);
+                }
+            }
+
+            return View("Index");
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(QuestionnaireViewModel customer)
+        {
+
+            return RedirectToAction("Index", "Questionnaire");
+        }
+
     }
 }
